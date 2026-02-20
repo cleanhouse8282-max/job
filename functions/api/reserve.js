6f185cfcb3210c1a3e0b088ba4ed2de126fb5ded
cleanhouse8282-max <cleanhouse8282@gmail.com>
2026-02-20 17:24:09 +0900

    Update reserve.js

--- functions/api/reserve.js
+++ functions/api/reserve.js
@@ -3,9 +3,9 @@ export async function onRequestPost(context){
 const formData=await context.request.formData();
 const {DB}=context.env;
 try{
-await DB.prepare("INSERT INTO reservations (name,phone,category,timeslot,roadAddress,detailAddress,pyeong,date,status) VALUES (?,?,?,?,?,?,?,?,?)")
+await DB.prepare("INSERT INTO reservations (name,phone,category,timeslot,roadAddress,detailAddress,pyeong,date,status,worker_id) VALUES (?,?,?,?,?,?,?,?,?,?)")
 .bind(formData.get("name"),formData.get("phone"),formData.get("category"),formData.get("timeslot"),
-formData.get("roadAddress"),formData.get("detailAddress"),formData.get("pyeong"),formData.get("date"),"pending").run();
+formData.get("roadAddress"),formData.get("detailAddress"),formData.get("pyeong"),formData.get("date"),"pending",formData("workerSelect").run();
 return new Response("OK");
 }catch{return new Response("Duplicate",{status:400});}
 }
