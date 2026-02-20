e89124db1fda0368d96b45af24ea7103cb644bae
cleanhouse8282-max <cleanhouse8282@gmail.com>
2026-02-20 16:50:37 +0900

    Update workers.js

--- functions/api/workers.js
+++ functions/api/workers.js
@@ -1,25 +1,25 @@
 //export async function onRequest() {
 //  return Response.json([{id:1, name:"테스트작업자"}]);
 //}
-//export async function onRequest({ env }) {
-//  const db = env.mydb;
+export async function onRequest({ env }) {
+  const db = env.mydb;
 
-//  const result = await db.prepare(`
-//    SELECT id, name
-//    FROM workers
-//    WHERE active = 1
-//    ORDER BY name ASC
-//  `).all();
+  const {result} = await db.prepare(`
+    SELECT id, name
+    FROM workers
+    WHERE active = 1
+    ORDER BY name ASC
+  `).all();
 
-//  return Response.json(result.results);
-//}
-
-export async function onRequest(context){
-if(!auth(context.request)) return new Response("Unauthorized",{status:401});
-const {DB}=context.env;
-const {results}=await DB.prepare("SELECT id, name FROM workers ORDER BY name").all();
-return Response.json(results);
+  return Response.json(result.results);
 }
+
+//export async function onRequest(context){
+//if(!auth(context.request)) return new Response("Unauthorized",{status:401});
+//const {DB}=context.env;
+//const {results}=await DB.prepare("SELECT id, name FROM workers ORDER BY name").all();
+//return Response.json(results);
+//}
 
 //export async function onRequest({ env }) {
 //  const db = env.mydb;
