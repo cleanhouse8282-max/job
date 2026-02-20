ca2de750c4572e9f48506579ade33ab9236355eb
cleanhouse8282-max <cleanhouse8282@gmail.com>
2026-02-20 17:16:56 +0900

    Update workers.js

--- functions/api/workers.js
+++ functions/api/workers.js
@@ -5,11 +5,11 @@
 //}
 
 
-function auth(req){return req.headers.get("Authorization")==="Basic "+btoa("admin:admin");}
+// function auth(req){return req.headers.get("Authorization")==="Basic "+btoa("admin:admin");}
 export async function onRequestGet(context){
 if(!auth(context.request)) return new Response("Unauthorized",{status:401});
 const {DB}=context.env;
-const {results}=await DB.prepare("SELECT * FROM reservations ORDER BY date").all();
+const {results}=await DB.prepare("SELECT id,name FROM reservations ORDER BY date").all();
 return Response.json(results);
 }
 
