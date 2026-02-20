49b0908bb79368cca0868ff8b4f6bc0b5271d05a
cleanhouse8282-max <cleanhouse8282@gmail.com>
2026-02-20 15:36:52 +0900

    Update workers.js

--- functions/workers.js
+++ functions/workers.js
@@ -1,11 +1,3 @@
-export async function onRequest({ request, env }) {
-  const db = env.mydb;
-
-  const result = await db.prepare(`
-    SELECT id, name
-    FROM workers
-    ORDER BY name ASC
-  `).all();
-
-  return Response.json(result.results);
+export async function onRequest() {
+  return Response.json([{id:1, name:"테스트작업자"}]);
 }
